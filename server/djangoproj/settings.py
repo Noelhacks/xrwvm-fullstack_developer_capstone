3b9d50aa79f5417a5454c4965ac34bde125659a5
Noelhacks <153246781+Noelhacks@users.noreply.github.com>
2024-05-02 16:22:21 -0500

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 
-                 'https://nyanoe772-8000.theiadockernext-1-labs-prod-
+                 'https://
+                 nyanoe772-8000.theiadockernext-1-labs-prod-
                  theiak8s-4-tor01.proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = ['https://nyanoe772-8000.theiadockernext-1-labs-prod-
 theiak8s-4-tor01.proxy.cognitiveclass.ai']
