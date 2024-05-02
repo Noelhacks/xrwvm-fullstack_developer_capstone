9dec97af0b8d586a5e1d84e8f5883d8fcdf557e1
Noelhacks <153246781+Noelhacks@users.noreply.github.com>
2024-05-02 16:26:38 -0500

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,11 +28,8 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 
-                 'https://nyanoe772-8000.theiadockernext-1-labs-
-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://nyanoe772-8000.theiadockernext-1-labs-prod-
-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = ['']
+CSRF_TRUSTED_ORIGINS = ['']
 
 
 
